43b730aa04428cedcb88fa81c1897f2c9ac911b8
Reed Rudkin <rrudkin1@gmail.com>
2021-07-28 07:44:41 -0600

    updated script and it works now

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
     var csv = document.getElementById('CSV');
     var readFileEx = document.getElementById('processFilesButton');
 
-    let resultsArr = [];
+
     function readFile() {
         Papa.parse(csv.files[0], {
             complete: function(results) {
-            resultsArr.push(results);
+
+
+                if(results.data[0].hasOwnProperty('Fee')) {
+                // if(Object.entries(results.data[0]).length === 18) {
+                    console.log('Results:');
+                    console.log(results);
+                    console.log(Object.entries(results.data[0]).length);
+            } else {
+                alert("It looks like you may have uploaded the wrong CSV file...")
+                }
             },
             header: true, 
             skipEmptyLines: 'greedy'
         });
-        console.log('Results:');
-        console.log(resultsArr[0]);
     }
 
     readFileEx.addEventListener('click', readFile);
-
-
-    
