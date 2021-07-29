f193435aba2e3ea01fe58b8f61dfcb40e38cf559
Reed Rudkin <rrudkin1@gmail.com>
2021-07-29 07:41:28 -0600

    various updatses to script and css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
     var csv = document.getElementById('CSV');
     var readFileEx = document.getElementById('processFilesButton');
+    var statusBar = document.querySelector('.status-bar');
 
 
     function readFile() {
@@ -9,11 +10,18 @@
 
                 if(results.data[0].hasOwnProperty('Fee')) {
                 // if(Object.entries(results.data[0]).length === 18) {
+                    statusBar.classList.remove('fail');
+                    statusBar.classList.add('success');
+                    statusBar.innerHTML = 'Success!'
                     console.log('Results:');
                     console.log(results);
                     console.log(Object.entries(results.data[0]).length);
-            } else {
-                alert("It looks like you may have uploaded the wrong CSV file...")
+                } else {
+                    statusBar.classList.remove('success');
+                    statusBar.classList.add('fail');
+                    statusBar.innerHTML = 'Failed...'
+                
+                setTimeout(function() { alert("It looks like you uploaded the wrong CSV file..."); }, 500);
                 }
             },
             header: true, 
