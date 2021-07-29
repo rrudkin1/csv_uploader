523972d1ec8e9546f426abf6aae4d1a3ab92df09
Reed Rudkin <rrudkin1@gmail.com>
2021-07-29 14:53:36 -0600

    testing alerts and feedback

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
     var csv = document.getElementById('CSV');
     var readFileEx = document.getElementById('processFilesButton');
     var statusBar = document.querySelector('.status-bar');
+    var processButton = document.querySelector('.process-btn');
 
 
     function readFile() {
@@ -10,18 +11,21 @@
 
                 if(results.data[0].hasOwnProperty('Fee')) {
                 // if(Object.entries(results.data[0]).length === 18) {
-                    statusBar.classList.remove('fail');
-                    statusBar.classList.add('success');
-                    statusBar.innerHTML = 'Success!'
+                    processButton.classList.remove('fail');
+                    processButton.classList.add('success');
+                    processButton.innerHTML = '<i class="fas fa-check"></i> Success';
+                    // processButton.innerHTML = 
                     console.log('Results:');
                     console.log(results);
                     console.log(Object.entries(results.data[0]).length);
                 } else {
-                    statusBar.classList.remove('success');
-                    statusBar.classList.add('fail');
-                    statusBar.innerHTML = 'Failed...'
+                    processButton.classList.remove('success');
+                    processButton.classList.add('fail');
+                    processButton.innerHTML = '<i class="fas fa-times"></i> Failed';
                 
-                setTimeout(function() { alert("It looks like you uploaded the wrong CSV file..."); }, 500);
+                setTimeout(function() { alert("It looks like you uploaded the wrong file... Make sure you are loading a .csv file, and make sure to upload the base list and billed list in the correct locations."); }, 500);
+
+                setTimeout(function() { processButton.classList.remove('fail'), processButton.innerHTML = 'Process Files'; }, 4000);
                 }
             },
             header: true, 
