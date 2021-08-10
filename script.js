9ef226482015983505cc199638edd5f2eb0e33a1
Reed Rudkin <rrudkin1@gmail.com>
2021-08-10 16:23:02 -0600

    experimented more with script js and formatted

--- script.js
+++ script.js
@@ -1,56 +1,62 @@
-    var csv = document.getElementById('CSV');
-    var readFileEx = document.getElementById('processFilesButton');
-    var processButton = document.querySelector('.process-btn');
+var csv = document.getElementById('CSV')
+var readFileEx = document.getElementById('processFilesButton')
+var processButton = document.querySelector('.process-btn')
 
+function readFile() {
+  Papa.parse(csv.files[0], {
+    complete: function (results) {
+      if (csv.value.split('.').pop() != 'csv') {
+        processButton.classList.remove('success')
+        processButton.classList.add('fail')
+        processButton.innerHTML = 'Failed'
+        setTimeout(function () {
+          alert(
+            'It looks like you may have uploaded the wrong file type. Make sure to upload .csv files only.'
+          )
+        }, 500)
+        setTimeout(function () {
+          window.location = '/'
+        }, 4000)
+      } else if (results.data[0].hasOwnProperty('Fee')) {
+        // if(Object.entries(results.data[0]).length === 18) {
+        processButton.classList.remove('success')
+        processButton.classList.add('fail')
+        processButton.innerHTML = 'Failed'
+        setTimeout(function () {
+          alert(
+            'It looks like you may have uploaded your file(s) in the wrong location. Make sure to upload the base list and billed list in the correct locations.'
+          )
+        }, 500)
+        setTimeout(function () {
+          window.location = '/'
+        }, 4000)
+      } else {
+        // RR: if passes both tests = success
+        processButton.classList.remove('fail')
+        processButton.classList.add('success')
+        processButton.innerHTML = 'Success'
+        returnedData = results
 
-    function readFile() {
-        Papa.parse(csv.files[0], {
-            complete: function(results) {
-                if(csv.value.split('.').pop() != 'csv') {
-                    processButton.classList.remove('success');
-                    processButton.classList.add('fail');
-                    processButton.innerHTML = 'Failed';
-                    setTimeout(function() { alert("It looks like you may have uploaded the wrong file type. Make sure to upload .csv files only."); }, 500);
-                    setTimeout(function() { window.location = '/'; }, 4000);
-                } else if(results.data[0].hasOwnProperty('Fee')) {
-                // if(Object.entries(results.data[0]).length === 18) {
-                    processButton.classList.remove('success');
-                    processButton.classList.add('fail');
-                    processButton.innerHTML = 'Failed';
-                    setTimeout(function() { alert("It looks like you may have uploaded your file(s) in the wrong location. Make sure to upload the base list and billed list in the correct locations."); }, 500);
-                    setTimeout(function() { window.location = '/'; }, 4000);
-                } else {
-                    // RR: if passes both tests = success
-                    processButton.classList.remove('fail');
-                    processButton.classList.add('success');
-                    processButton.innerHTML = 'Success';
-                    returnedData = results; 
+        let arrOfObjs = []
+        for (i = 0; i < 10; i++) {
+          arrOfObjs.push(returnedData.data[i])
+        }
 
-                    let arrOfObjs = [];
-                    for(i = 0; i < 10; i++) {
-                        arrOfObjs.push(returnedData.data[i]);
-                    };
+        let totalValArray = []
+        for (i = 0; i < arrOfObjs.length; i++) {
+          totalValArray.push(Number(arrOfObjs[i].TotalAccountValue))
+        }
 
-                    let totalValArray = [];
-                    for(i = 0; i < arrOfObjs.length; i++ ) {
-                        totalValArray.push(Number(arrOfObjs[i].TotalAccountValue));
-                    }
+        let totalVal = totalValArray.reduce((a, b) => a + b, 0)
 
-                    let totalVal = 0
-                    for(i = 0; i < totalValArray.length; i++) {
-                        totalVal += totalValArray[i];
-                    }
-           
-                    // console.log(arrOfObjs);
-                    // console.log(totalValArray);
-                    // console.log(totalVal);
+        console.log(arrOfObjs)
+        console.log(totalValArray)
+        console.log(totalVal)
 
-                    createTable(arrOfObjs);
+        function createTable(data) {
+          let table = document.getElementById('myTable')
 
-                    function createTable(data) {
-                        let table = document.getElementById('myTable');
-                        
-                        table.innerHTML = `
+          table.innerHTML = `
                             <table class="table table-striped table-hover">
                                 <thead>
                                     <tr>
@@ -64,68 +70,74 @@
 
                                 </tbody>
                             </table>
-                        `;
+                        `
 
-                        for(i = 0; i < data.length; i++) {
-                            $('#myTableBody').append(`<tr>
+          for (i = 0; i < data.length; i++) {
+            $('#myTableBody').append(`<tr>
                                     <td>${data[i].Account}</td>
                                     <td>${data[i].AccountName}</td>
-                                    <td>${new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD' }).format(data[i].TotalAccountValue)}</td>
+                                    <td>${new Intl.NumberFormat('us-US', {
+                                      style: 'currency',
+                                      currency: 'USD',
+                                    }).format(data[i].TotalAccountValue)}</td>
                                     <td><button class="delete-button" id='deleteRowBtn'><i class="fas fa-times"></i></button></td> 
                                 </tr>
-                                `);
-                            }
- 
-                            $('#myTableBody').append(`
+                                `)
+          }
+
+          $('#myTableBody').append(`
                             <tr>
                                 <td colspan="2"><strong>Total missed account value:</strong></td>
-                                <td colspan="2"><strong>${new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD' }).format(totalVal)}</strong></td>
+                                <td colspan="2"><strong>${new Intl.NumberFormat(
+                                  'us-US',
+                                  { style: 'currency', currency: 'USD' }
+                                ).format(totalVal)}</strong></td>
                             </tr>
-                            `);
-                        };
-                        
-                    $("#myTable").on('click', '.delete-button', function() {
-                        let getID = $(this).closest('tr').find('td:first');
-                        let acctNum = getID[0].outerText;
-                        for(i = 0; i < arrOfObjs.length; i++) {
-                            if(arrOfObjs[i].Account == acctNum) {
-                                arrayOfObjs = arrOfObjs.splice(i, 1);
-                            };
-                        };
-                        
-                        for(i = 0; i < arrOfObjs.length; i++ ) {
-                            totalValArray.push(Number(arrOfObjs[i].TotalAccountValue));
-                        }
-                        
-                        for(i = 0; i < totalValArray.length; i++) {
-                            totalVal += totalValArray[i];
-                        }
-                        
-                        createTable(arrOfObjs);
+                            `)
+        }
 
-                    })
-                };   
-            },
+        createTable(arrOfObjs)
 
-            header: true, 
-            skipEmptyLines: 'greedy',
-            transform: dataRegex,
-            transformHeader: (h) => {return h.replaceAll(/\s/g, '');}
-        });
-    };
+        $('#myTable').on('click', '.delete-button', function () {
+          let getID = $(this).closest('tr').find('td:first')
+          let acctNum = getID[0].outerText
+          for (i = 0; i < arrOfObjs.length; i++) {
+            if (arrOfObjs[i].Account == acctNum) {
+              arrayOfObjs = arrOfObjs.splice(i, 1)
+            }
+          }
 
-    function dataRegex(v) {
-        let transformedString = v;
-        let dollarSignRegex = /\$(\d)/g;
-        let commaRegex = /(\d)\,(\d)/g;
-        let trailingSpaceRegex = /$\s/g;
+          for (i = 0; i < arrOfObjs.length; i++) {
+            totalValArray.push(Number(arrOfObjs[i].TotalAccountValue))
+          }
 
-        transformedString = transformedString.replaceAll(dollarSignRegex, '$1');
-        transformedString = transformedString.replaceAll(commaRegex, '$1$2');
-        transformedString = transformedString.replaceAll(trailingSpaceRegex, '');
+          createTable(arrOfObjs)
 
-        return transformedString;
-    };
+          totalVal = totalValArray.reduce((a, b) => a + b, 0)
+        })
+      }
+    },
 
-    readFileEx.addEventListener('click', readFile);
+    header: true,
+    skipEmptyLines: 'greedy',
+    transform: dataRegex,
+    transformHeader: (h) => {
+      return h.replaceAll(/\s/g, '')
+    },
+  })
+}
 
+function dataRegex(v) {
+  let transformedString = v
+  let dollarSignRegex = /\$(\d)/g
+  let commaRegex = /(\d)\,(\d)/g
+  let trailingSpaceRegex = /$\s/g
+
+  transformedString = transformedString.replaceAll(dollarSignRegex, '$1')
+  transformedString = transformedString.replaceAll(commaRegex, '$1$2')
+  transformedString = transformedString.replaceAll(trailingSpaceRegex, '')
+
+  return transformedString
+}
+
+readFileEx.addEventListener('click', readFile)
