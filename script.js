9fec345932f8b69a175c484886ffb4f9824f1ebf
Reed Rudkin <rrudkin1@gmail.com>
2021-08-11 13:51:46 -0600

    updated styling and formatting

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
-var csv = document.getElementById('CSV')
-var readFileEx = document.getElementById('processFilesButton')
-var processButton = document.querySelector('.process-btn')
-var downloadBtn = document.querySelector('.download-report-btn')
+var csv = document.getElementById('CSV');
+var readFileEx = document.getElementById('processFilesButton');
+var processButton = document.querySelector('.process-btn');
+var downloadBtn = document.querySelector('.download-report-btn');
 
 function readFile() {
   Papa.parse(csv.files[0], {
     complete: function (results) {
       if (csv.value.split('.').pop() != 'csv') {
-        processButton.classList.remove('success')
-        processButton.classList.add('fail')
-        processButton.innerHTML = 'Failed'
+        processButton.classList.remove('success');
+        processButton.classList.add('fail');
+        processButton.innerHTML = 'Failed';
         setTimeout(function () {
           alert(
             'It looks like you may have uploaded the wrong file type. Make sure to upload .csv files only.'
-          )
-        }, 500)
+          );
+        }, 500);
         setTimeout(function () {
-          window.location = '/'
-        }, 4000)
+          window.location = '/';
+        }, 4000);
       } else if (results.data[0].hasOwnProperty('Fee')) {
         // if(Object.entries(results.data[0]).length === 18) {
-        processButton.classList.remove('success')
-        processButton.classList.add('fail')
-        processButton.innerHTML = 'Failed'
+        processButton.classList.remove('success');
+        processButton.classList.add('fail');
+        processButton.innerHTML = 'Failed';
         setTimeout(function () {
           alert(
             'It looks like you may have uploaded your file(s) in the wrong location. Make sure to upload the base list and billed list in the correct locations.'
-          )
-        }, 500)
+          );
+        }, 500);
         setTimeout(function () {
-          window.location = '/'
-        }, 4000)
+          window.location = '/';
+        }, 4000);
       } else {
         // RR: if passes both tests = success
-        processButton.classList.remove('fail')
-        processButton.classList.add('success')
-        processButton.innerHTML = 'Success'
-        returnedData = results
+        processButton.classList.remove('fail');
+        processButton.classList.add('success');
+        processButton.innerHTML = 'Success';
+        returnedData = results;
 
-        let arrOfObjs = []
+        let arrOfObjs = [];
         for (i = 0; i < 10; i++) {
-          arrOfObjs.push(returnedData.data[i])
+          arrOfObjs.push(returnedData.data[i]);
         }
 
-        let totalValArray = []
+        let totalValArray = [];
         for (i = 0; i < arrOfObjs.length; i++) {
-          totalValArray.push(Number(arrOfObjs[i].TotalAccountValue))
+          totalValArray.push(Number(arrOfObjs[i].TotalAccountValue));
         }
 
-        let totalVal = totalValArray.reduce((a, b) => a + b, 0)
+        let totalVal = totalValArray.reduce((a, b) => a + b, 0);
 
         function createTable(data) {
-          let table = document.getElementById('myTable')
+          let table = document.getElementById('myTable');
 
           table.innerHTML = `
                             <table class="table table-striped table-hover">
@@ -67,7 +67,7 @@ function readFile() {
 
                                 </tbody>
                             </table>
-                        `
+                        `;
 
           for (i = 0; i < data.length; i++) {
             $('#myTableBody').append(`<tr>
@@ -81,7 +81,7 @@ function readFile() {
                                     </i>
                                     </i></button></td> 
                                 </tr>
-                                `)
+                                `);
           }
 
           $('#myTableBody').append(`
@@ -92,34 +92,34 @@ function readFile() {
                                   { style: 'currency', currency: 'USD' }
                                 ).format(totalVal)}</strong></td>
                             </tr>
-                            `)
+                            `);
         }
 
-        createTable(arrOfObjs)
+        createTable(arrOfObjs);
 
         downloadBtn.addEventListener('click', () => {
-          console.log(arrOfObjs)
-        })
+          console.log(arrOfObjs);
+        });
 
         $('#myTable').on('click', '.delete-button', function () {
-          let getID = $(this).closest('tr').find('td:first')
-          let acctNum = getID[0].outerText
+          let getID = $(this).closest('tr').find('td:first');
+          let acctNum = getID[0].outerText;
           for (i = 0; i < arrOfObjs.length; i++) {
             if (arrOfObjs[i].Account == acctNum) {
-              arrayOfObjs = arrOfObjs.splice(i, 1)
+              arrayOfObjs = arrOfObjs.splice(i, 1);
             }
           }
 
-          let totalValArray = []
+          let totalValArray = [];
 
           for (i = 0; i < arrOfObjs.length; i++) {
-            totalValArray.push(Number(arrOfObjs[i].TotalAccountValue))
+            totalValArray.push(Number(arrOfObjs[i].TotalAccountValue));
           }
 
-          totalVal = totalValArray.reduce((a, b) => a + b, 0)
+          totalVal = totalValArray.reduce((a, b) => a + b, 0);
 
-          createTable(arrOfObjs)
-        })
+          createTable(arrOfObjs);
+        });
       }
     },
 
@@ -127,22 +127,22 @@ function readFile() {
     skipEmptyLines: 'greedy',
     transform: dataRegex,
     transformHeader: (h) => {
-      return h.replaceAll(/\s/g, '')
+      return h.replaceAll(/\s/g, '');
     },
-  })
+  });
 }
 
 function dataRegex(v) {
-  let transformedString = v
-  let dollarSignRegex = /\$(\d)/g
-  let commaRegex = /(\d)\,(\d)/g
-  let trailingSpaceRegex = /$\s/g
+  let transformedString = v;
+  let dollarSignRegex = /\$(\d)/g;
+  let commaRegex = /(\d)\,(\d)/g;
+  let trailingSpaceRegex = /$\s/g;
 
-  transformedString = transformedString.replaceAll(dollarSignRegex, '$1')
-  transformedString = transformedString.replaceAll(commaRegex, '$1$2')
-  transformedString = transformedString.replaceAll(trailingSpaceRegex, '')
+  transformedString = transformedString.replaceAll(dollarSignRegex, '$1');
+  transformedString = transformedString.replaceAll(commaRegex, '$1$2');
+  transformedString = transformedString.replaceAll(trailingSpaceRegex, '');
 
-  return transformedString
+  return transformedString;
 }
 
-readFileEx.addEventListener('click', readFile)
+readFileEx.addEventListener('click', readFile);
