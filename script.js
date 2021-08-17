fac6715cda0e970b4396f9651aa18946d2ad365c
Reed Rudkin <rrudkin1@gmail.com>
2021-08-17 07:08:59 -0600

    created download csv functionality

--- script.js
+++ script.js
@@ -159,24 +159,15 @@ downloadBtn.addEventListener('click', () => {
   downloadedReport = Papa.unparse(newObjsList, { header: true });
   console.log(downloadedReport);
 
-  var blob = new Blob([downloadedReport], { type: 'text/csv;charset=utf-8;' });
-  if (navigator.msSaveBlob) {
-    // IE 10+
-    navigator.msSaveBlob(blob, filename);
-  } else {
-    var link = document.createElement('a');
-    if (link.download !== undefined) {
-      // feature detection
-      // Browsers that support HTML5 download attribute
-      var url = URL.createObjectURL(blob);
-      link.setAttribute('href', url);
-      link.setAttribute('download', filename);
-      link.style.visibility = 'hidden';
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
-  }
+  var downloadLink = document.createElement("a");
+  var blob = new Blob(["\ufeff", downloadedReport]);
+  var url = URL.createObjectURL(blob);
+  downloadLink.href = url;
+  downloadLink.download = "missedAccounts.csv";
+
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink)
 });
 
 readFileEx.addEventListener('click', readFile);
